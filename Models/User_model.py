fe02e86506ea44b5c9281817e610c2f3fb2595f7
ArjunPraja <prajapatiarjun2801@gmail.com>
2025-08-22 15:17:46 +0530

    Initial Commit

---
+++ Models/User_model.py
@@ -0,0 +1,17 @@
+import time
+import uuid  
+
+class Users:
+    def __init__(self, name, email, phone_no, role, password, user_id=None):
+       
+        self.id = user_id or str(uuid.uuid4())
+        self.name = name
+        self.email = email
+        self.phone_no = phone_no
+        self.role = role
+        self.password = password 
+        self.created_at = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+    def __str__(self):
+        return (f"User({self.id}, Name: {self.name}, Email: {self.email}, "
+                f"Role: {self.role}, Created At: {self.created_at})")
---
+++ Models/rides_model.py
@@ -0,0 +1,17 @@
+import time
+import uuid  
+
+class RidesModel:
+    def __init__(self, rider_id, driver_id, pickup, drop_location, status):
+        self.id = str(uuid.uuid4())  
+        self.rider_id = rider_id
+        self.driver_id = driver_id
+        self.pickup = pickup
+        self.drop = drop_location
+        self.status = status
+        self.created_at = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+    def __str__(self):
+        return (f"Ride({self.id}, Rider: {self.rider_id}, Driver: {self.driver_id}, "
+                f"Pickup: {self.pickup}, Drop: {self.drop}, Status: {self.status}, "
+                f"Created At: {self.created_at})")
